754bfc27c08bccc5c66672e9e79c3c311dd6dac5
AtSmartInsert <Administrator@USER-20170101RC>
2017-07-27 10:02:22 +0800

    ee

---
+++ src/com/atguigu/gibhubtest/GitHubTest.java
@@ -0,0 +1,10 @@
+package com.atguigu.gibhubtest;
+
+
+public class GitHubTest {
+
+    public static void main(String[] args) {
+        System.out.println("hello,GitHub!");
+        System.out.println("git@github.com:AtSmartInsert/GitHubTestProject.git");
+    }
+}
